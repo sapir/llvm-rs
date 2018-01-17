d381300b0bce89401edab06abe3bc2db8eb391f1
Rowlandson Pratt <striderman34@gmail.com>
2018-01-17 19:09:05 +0000

    export pass_manager

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,7 @@ pub use types::*;
 pub use value::{Alias, Arg, Attribute, Function, GlobalValue, GlobalVariable, Linkage, Predicate,
                 Value};
 pub use util::Sub;
+pub use pass_manager::{PassManger,PassMangerBuilder}
 
 pub mod prelude {
 
